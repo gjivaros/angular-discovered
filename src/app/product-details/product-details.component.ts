194c0b7d4bc603269d396de73f34393f8cf76843
MrJivaros <69278829+MrJivaros@users.noreply.github.com>
2022-10-25 21:43:09 +0100

    work on product detail component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products, Product } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product: Product | undefined;
+  constructor(route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const params = this.route.snapshop;
   }
-
 }
